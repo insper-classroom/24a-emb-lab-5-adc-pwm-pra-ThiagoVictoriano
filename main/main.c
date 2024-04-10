fe131910e65e555a907d72dd2bc4ac047d718407
ThiagoVictoriano <thiagovic03@gmail.com>
2024-04-10 14:20:43 +0000

    funcionando

--- main/main.c
+++ main/main.c
@@ -1,17 +1,11 @@
-/*
- * LED blink with FreeRTOS
- */
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
 
 #include "pico/stdlib.h"
-#include <stdio.h>
 #include "hardware/adc.h"
-
-#include <math.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 QueueHandle_t xQueueAdc;
 
@@ -20,20 +14,87 @@ typedef struct adc {
     int val;
 } adc_t;
 
+void write_package(adc_t data) {
+    int val = data.val;
+    int msb = val >> 8;
+    int lsb = val & 0xFF ;
+
+    uart_putc_raw(uart0, data.axis);
+    uart_putc_raw(uart0, lsb);
+    uart_putc_raw(uart0, msb);
+    uart_putc_raw(uart0, -1);
+}
+
+void x_task(void *p) {
+    adc_gpio_init(27);
+    adc_t data;
+    data.axis = 0;
+    int deadzone_min = -160;
+    int deadzone_max = 160;
+    int filtered[5];
+    int sum = 0;
+
+    while(1){
+        adc_select_input(1);
+        int result = adc_read();
+
+        
+        int average = (result - 2048)/8;
+
+        // Verifica se o valor está dentro da zona morta
+        if (average < deadzone_min || average > deadzone_max)
+        {
+            data.val = average/2;
+            xQueueSendFromISR(xQueueAdc, &data, 0);
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void y_task(void *p) { 
+    adc_gpio_init(26);
+    adc_t data;
+    data.axis = 1;
+    int deadzone_min = -160;
+    int deadzone_max = 160;
+    int filtered[5];
+    int sum = 0;
+
+    while(1){
+        adc_select_input(0);
+        int result = adc_read();
+
+        
+        int average = (result - 2048)/8;
+
+        // Verifica se o valor está dentro da zona morta
+        if (average < deadzone_min || average > deadzone_max)
+        {
+            data.val = average/2;
+            xQueueSendFromISR(xQueueAdc, &data, 0);
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
 
 void uart_task(void *p) {
     adc_t data;
-
+    uint8_t buffer[4];
     while (1) {
         xQueueReceive(xQueueAdc, &data, portMAX_DELAY);
+        write_package(data);
     }
 }
 
 int main() {
     stdio_init_all();
 
+    adc_init();
+
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
+    xTaskCreate(x_task, "x_task", 4096, NULL, 1, NULL);
+    xTaskCreate(y_task, "y_task", 4096, NULL, 1, NULL);
     xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
